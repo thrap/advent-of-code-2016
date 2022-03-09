0e8c1fec08672994d934ee12d73f84b20cc6748a
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-09 19:54:48 +0100

    Solve day 1 part 2

--- src/day01/index.js
+++ src/day01/index.js
@@ -2,9 +2,10 @@ import run from "aocrunner"
 
 const parseInput = rawInput => rawInput.split(', ').map(x => [x[0], +x.substr(1)])
 
+const dirs = [[1,0],[0,1],[-1,0],[0,-1]]
+
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
-  const dirs = [[1,0],[0,1],[-1,0],[0,-1]]
   var dir = 0
   var x = 0
   var y = 0
@@ -20,8 +21,24 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  var dir = 0
+  var x = 0
+  var y = 0
+  const visit = {'0,0': true}
+  input.find(([turn, steps]) => {
+    dir = (4 + dir + (turn == 'R' ? -1 : 1)) % 4
+    const [dx, dy] = dirs[dir]
+    for (var i = 0; i < steps; i++) {
+      x += dx
+      y += dy
+      if (visit[[x,y]]) {
+        return true
+      }
+      visit[[x,y]] = true
+    }
+  })
 
-  return
+  return Math.abs(x) + Math.abs(y)
 }
 
 run({
