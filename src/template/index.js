2cb1f48762b582e6ad45bdc845819371d1eb7b98
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-10 10:59:18 +0100

    Update template

--- src/template/index.js
+++ src/template/index.js
@@ -2,7 +2,7 @@ import run from "aocrunner"
 
 const re = /(.*)/
 const parseLine = l => l.match(re).slice(1).map(x => +x ? +x : x)
-const parseInput = rawInput => rawInput//.split('\n')//.map(parseLine)
+const parseInput = rawInput => rawInput.split('\n')//.map(parseLine)
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
@@ -22,13 +22,13 @@ const part2Input = part1Input
 run({
   part1: {
     tests: [
-      { input: part1Input, expected: "" }
+      { input: part1Input, expected: '' },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      { input: part2Input, expected: "" }
+      { input: part2Input, expected: '' },
     ],
     solution: part2,
   },
