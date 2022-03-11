0513267ef50e765c9081d0ee9f145bfb28363aa1
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-11 17:10:33 +0100

    Add brute force for day 19 part 2

--- src/day19/index.js
+++ src/day19/index.js
@@ -16,26 +16,55 @@ const part1 = (rawInput) => {
   return elf.num
 }
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+const part2brute = (rawInput) => {
+  const elves = parseInput(rawInput)
 
-  return
+  const arr = Array(elves).fill(0).map((_,i) => ({num: i+1, left: (i+1) % elves}))
+
+  //console.log(arr);
+  var remainding = elves
+  var elf = arr[0]
+  for(var i = 0; remainding > 0; i++) {
+    if (remainding % 1000 == 0) {
+      console.log(remainding);
+    }
+    var across = Math.floor(remainding/2)
+    var goalElf = elf
+    for (var j = 0; j < across-1; j++) {
+      goalElf = arr[goalElf.left]
+    }
+    if (arr[goalElf.left] == elf) {
+      return elf.num
+    }
+    /*console.log("steps across", across, remainding);
+    console.log(elf.num, arr[goalElf.left].num);
+    console.log();*/
+    goalElf.left = arr[goalElf.left].left
+    remainding--
+    elf = arr[elf.left]
+  }
+
+  return elf.num
 }
 
-const part1Input = `5`
-const part2Input = part1Input
+const part2 = (rawInput) => {
+  return part2brute(rawInput)
+}
+
 run({
   part1: {
     tests: [
-      { input: part1Input, expected: '' },
+      { input: '5', expected: 3 },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      { input: part2Input, expected: '' },
+      { input: '5', expected: 2 },
+      { input: '999', expected: 270 },
+      { input: '83741', expected: 24692 },
     ],
     solution: part2,
   },
-  onlyTests: false,
+  onlyTests: true
 })
