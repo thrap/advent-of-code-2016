ee193042a5e509a12ba9730d5c4f0fb5b3c59335
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-11 12:12:40 +0100

    Solve day 14 part 2

--- README.md
+++ README.md
@@ -24,7 +24,7 @@
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
-[![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%86/yellow)](src/day14)
+[![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
@@ -160,9 +160,9 @@ Both parts: 24.564940999999997ms
 
 ```
 Day 14
-Time part 1: 105.849ms
-Time part 2: -
-Both parts: 105.84909ms
+Time part 1: 99.418ms
+Time part 2: 262.236ms
+Both parts: 361.654161ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 27/50
-Total time: 113483.623ms
+Total stars: 28/50
+Total time: 113739.428ms
 ```
 
 <!--/RESULTS-->
--- src/day14/index.js
+++ src/day14/index.js
@@ -1,15 +1,17 @@
 import run from "aocrunner"
 import MD5 from "crypto-js/md5.js"
+import fs from 'fs'
 
 const parseInput = rawInput => rawInput
+
+var long = /(.)\1{4,}/
+var short = /(.)\1{2,}/
 
 const part1 = (rawInput) => {
   const secret = parseInput(rawInput)
 
   const idx = '0123456789abcdef'.split('').reduce((acc, c) => ({...acc, [c]: []}), {})
 
-  var long = /(.)\1{4,}/
-  var short = /(.)\1{2,}/
   var keys = new Set()
   for (var i = 0; keys.size < 64; i++) {
     var hash = MD5(secret + i).toString()
@@ -31,26 +33,38 @@ const part1 = (rawInput) => {
   return Math.max(...keys)
 }
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+const part2 = () => {
+  var hashes = fs.readFileSync('hashes.txt','utf8').split('\n')
 
-  return
+  const isValid = (n, c) => {
+    for(var i = 1; i <= 1000; i++) {
+      if (new RegExp(c+c+c+c+c).test(hashes[n+i]))
+        return true
+    }
+    return false
+  }
+
+  var keys = []
+  for (var i = 0; keys.length < 64; i++) {
+    var hash = hashes[i]
+
+    if (short.test(hash)) {
+      const char = hash.match(short)[0][0]
+
+      if (isValid(i, char)) {
+        keys.push(i)
+      }
+    }
+  }
+
+  return keys[63]
 }
 
-const part1Input = `abc`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 22728 },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
