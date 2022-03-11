f7e48ea756e21ff460b5a2536c35a4be287f62cb
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-11 18:20:04 +0100

    Solve day 20 part 2

--- src/day20/index.js
+++ src/day20/index.js
@@ -15,25 +15,27 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
+  const input = parseInput(rawInput)
+  input.sort((a, b) => a[0] - b[0])
 
+  var count = 0
+  var max = 0
+  for (var i = 0; i < input.length-1; i++) {
+
+    var possible = Math.max(input[i][1] + 1, max)
+    max = Math.max(input[i][1] + 1, max)
+    if (possible < input[i+1][0]) {
+      count += input[i+1][0] - possible
+    }
+  }
+  return count
 }
 
-const part1Input = `5-8
-0-2
-4-7`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 3 },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
