36b4772b9b2240647693d3a6088edc2ae5c3ecaf
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-11 12:35:30 +0100

    Solve day 15 part 2

--- README.md
+++ README.md
@@ -25,7 +25,7 @@
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
-[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%86/yellow)](src/day15)
+[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
@@ -167,9 +167,9 @@ Both parts: 411.23552400000005ms
 
 ```
 Day 15
-Time part 1: 39.555ms
-Time part 2: -
-Both parts: 39.554682ms
+Time part 1: 40.077ms
+Time part 2: 273.711ms
+Both parts: 313.787336ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 29/50
-Total time: 113828.564ms
+Total stars: 30/50
+Total time: 114102.796ms
 ```
 
 <!--/RESULTS-->
--- src/day15/index.js
+++ src/day15/index.js
@@ -2,11 +2,9 @@ import run from "aocrunner"
 
 const re = /has (\d+) p.* (\d+)\./
 const parseLine = l => l.match(re).slice(1).map(x => +x ? +x : x)
-const parse = input => input.split('\n').map(parseLine)
+const discs = input => input.split('\n').map(parseLine)
 
-const part1 = (input) => {
-  const discs = parse(input)
-
+const timing = discs => {
   for (var offset = 0; true; offset++) {
     const positions = discs.map(([length, pos], i) => (pos + i + 1 + offset) % length)
     if (positions.every(x => x == 0)) {
@@ -15,27 +13,15 @@ const part1 = (input) => {
   }
 }
 
-const part2 = (input) => {
-  const discs = parse(input)
+const part1 = (input) => timing(discs(input))
 
-  return
-}
+const part2 = (input) => timing(discs(input).concat([[11, 0]]))
 
-const part1Input = `Disc #1 has 5 positions; at time=0, it is at position 4.
-Disc #2 has 2 positions; at time=0, it is at position 1.`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: '' },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
