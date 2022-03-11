fb07069646c06ba34ad69671322f7ec28d12456a
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-11 13:15:00 +0100

    Solve day 16 part 2

--- src/day16/index.js
+++ src/day16/index.js
@@ -1,12 +1,11 @@
 import run from "aocrunner"
 
+const step = a => {
+  const b = a.split('').reverse().map(c => c == '0' ? 1 : 0).join('')
+  return a + 0 + b
+}
+
 const part1 = (state) => {
-
-  const step = a => {
-    const b = a.split('').reverse().map(c => c == '0' ? 1 : 0).join('')
-    return a + 0 + b
-  }
-
   const disc = 272
   while (state.length < disc) {
     state = step(state)
@@ -18,15 +17,31 @@ const part1 = (state) => {
   }
 
   var sum = state.substring(0, disc)
+
   while(sum.length % 2 == 0) {
     sum = checksum(sum)
   }
-
   return sum
 }
 
-const part2 = (input) => {
+const part2 = (state) => {
+  const disc = 35651584
+  while (state.length < disc) {
+    state = step(state)
+  }
 
+  var sum = state.substring(0, disc)
+  while (sum.length % 2 == 0) {
+    var newSum = ''
+    for (var i = 0; i < sum.length; i+=2) {
+      if (sum[i] == sum[i+1])
+        newSum += '1'
+      else
+        newSum += '0'
+    }
+    sum = newSum
+  }
+  return sum
 }
 
 run({
